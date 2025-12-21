ebfa0596c68a4f81827a966ff0010927ebdc376a
Guilherme Carnietto Depizol <24928611+guiiicd@users.noreply.github.com>
2025-12-21 09:13:27 -0400

    Disable kick matchsetup

--- EventHandlers.cs
+++ EventHandlers.cs
@@ -28,10 +28,10 @@ public partial class MatchZy
                     CsTeam team = GetPlayerTeam(player);
                     if (team == CsTeam.None)
                     {
-                        Log($"[EventPlayerConnectFull] KICKING PLAYER STEAMID: {steamId}, Name: {player.PlayerName} (NOT ALLOWED!)");
-                        PrintToAllChat($"Kicking player {player.PlayerName} - Not a player in this game.");
-                        KickPlayer(player);
-                        return HookResult.Continue;
+                        // Log($"[EventPlayerConnectFull] KICKING PLAYER STEAMID: {steamId}, Name: {player.PlayerName} (NOT ALLOWED!)");
+                        // PrintToAllChat($"Kicking player {player.PlayerName} - Not a player in this game.");
+                        // KickPlayer(player);
+                        // return HookResult.Continue;
                     }
                 }
             }
